50351d6eee216f5145c306994f8e639b5fa1c30f
illia <46092983+bronewi4ok@users.noreply.github.com>
2025-09-10 14:21:36 +0300

    feat(layout) extend MainLayout with sidebar and header

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -1,9 +1,23 @@
+import { useCurrentPageTitle } from '@/app/router/model/helpers'
+import { AppHeader } from '@/widgets/AppHeader'
+import MainMenu from '@/widgets/Sidebar/ui/MainMenu'
 import { Outlet } from 'react-router-dom'
 
 export function MainLayout() {
+  const title = useCurrentPageTitle()
+
   return (
-    <>
-      <Outlet />
-    </>
+    <div className="flex flex-wrap items-stretch min-h-dvh w-dvw bg-back-500 text-dark break-all">
+      <aside className="felx-none bg-light">
+        <MainMenu />
+      </aside>
+
+      <main className="flex-1 bg-light flex flex-col min-h-dvh">
+        <AppHeader title={title} />
+        <div className="flex-1">
+          <Outlet />
+        </div>
+      </main>
+    </div>
   )
 }
