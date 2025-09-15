26064bfac17981b90749661f1ba3b12b0b234400
illia <46092983+bronewi4ok@users.noreply.github.com>
2025-09-15 11:56:34 +0300

    refactor(MainList): remove MainListFallback

--- src/shared/ui/mainList/index.ts
+++ src/shared/ui/mainList/index.ts
@@ -1,4 +1,3 @@
 export { MainList } from './MainList'
 export { MainListItem } from './MainListItem'
-export { MainListFallback } from './MainListFallback'
 export type { MainListItemProps, MainListProps } from './types'
