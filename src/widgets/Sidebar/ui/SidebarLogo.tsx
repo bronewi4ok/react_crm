51126d2bb57e016d99cf240d4a7d3c9effffc68c
illia <bronewi4ok@gmail.com>
2025-12-03 13:29:01 +0200

    ref: some small changes in widgets and projects

--- src/widgets/Sidebar/ui/SidebarLogo.tsx
+++ src/widgets/Sidebar/ui/SidebarLogo.tsx
@@ -6,7 +6,7 @@ export function SidebarLogo() {
   return (
     <Link
       to={mainRoutes.home.navPath}
-      className="flex items-center gap-6 font-bold p-6 "
+      className="flex items-center gap-6 font-bold p-6"
       aria-label="betaCRM logo">
       <Icon
         name="common-logo"
